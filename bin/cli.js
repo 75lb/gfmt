1371073b289b30a6d620ea590552e55a8486286a
Lloyd Brookes <lloyd.brookes@gmail.com>
2015-10-21 17:33:16 +0100

    rewrite

--- bin/cli.js
+++ bin/cli.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
-"use strict";
-var gfmTable = require("../");
-var collectJson = require("collect-json");
+'use strict'
+var gfmTable = require('../')
+var collectJson = require('collect-json')
 
 process.stdin
-    .pipe(collectJson(function(json){
-        return gfmTable(json).getTable();
-    }))
-    .pipe(process.stdout);
+  .pipe(collectJson(function (json) {
+    if (Array.isArray(json)) {
+      return gfmTable(json)
+    } else {
+      return gfmTable(json.data, json.columns)
+    }
+
+  }))
+  .pipe(process.stdout)
--- test/test.js
+++ test/test.js
@@ -1,62 +1,62 @@
-var test = require("tape");
-var gfmt = require("../");
+var test = require('tape')
+var gfmt = require('../')
 
-test(".getTable()", function(t){
-    var result = gfmt([
-        { "date": "10 Jun 2015", "downloads": 100 },
-        { "date": "11 Jun 2015", "downloads": 120 },
-        { "date": "12 Jun 2015", "downloads": 150 },
-        { "date": "13 Jun 2015", "downloads": 120 },
-        { "date": "14 Jun 2015", "downloads": 110 }
-    ]).getTable();
-    var expected = 
-"| date        | downloads |\n\
+test('.getTable()', function (t) {
+  var result = gfmt([
+    { 'date': '10 Jun 2015', 'downloads': 100 },
+    { 'date': '11 Jun 2015', 'downloads': 120 },
+    { 'date': '12 Jun 2015', 'downloads': 150 },
+    { 'date': '13 Jun 2015', 'downloads': 120 },
+    { 'date': '14 Jun 2015', 'downloads': 110 }
+  ]).getTable()
+  var expected =
+  '| date        | downloads |\n\
 | ----------- | --------- |\n\
 | 10 Jun 2015 | 100       |\n\
 | 11 Jun 2015 | 120       |\n\
 | 12 Jun 2015 | 150       |\n\
 | 13 Jun 2015 | 120       |\n\
-| 14 Jun 2015 | 110       |\n";
-    t.strictEqual(result, expected);
-    t.end();
-});
+| 14 Jun 2015 | 110       |\n'
+  t.strictEqual(result, expected)
+  t.end()
+})
 
-test(".getTable() with empty column", function(t){
-    var result = gfmt([
-        { "name": "Lloyd", "age": undefined },
-        { "name": "Roger", "age": undefined },
-        { "name": "Amir", "age": undefined },
-        { "name": "Frank", "age": undefined },
-        { "name": "Amy", "age": undefined }
-    ]).getTable();
-    var expected = 
-"| name  |\n\
+test('.getTable() with empty column', function (t) {
+  var result = gfmt([
+    { 'name': 'Lloyd', 'age': undefined },
+    { 'name': 'Roger', 'age': undefined },
+    { 'name': 'Amir', 'age': undefined },
+    { 'name': 'Frank', 'age': undefined },
+    { 'name': 'Amy', 'age': undefined }
+  ]).getTable()
+  var expected =
+  '| name  |\n\
 | ----- |\n\
 | Lloyd |\n\
 | Roger |\n\
 | Amir  |\n\
 | Frank |\n\
-| Amy   |\n";
-    t.strictEqual(result, expected);
-    t.end(); 
-});
+| Amy   |\n'
+  t.strictEqual(result, expected)
+  t.end()
+})
 
-test(".getTable() with empty column 2", function(t){
-    var result = gfmt([
-        { "name": "Lloyd", "age": null },
-        { "name": "Roger", "age": null },
-        { "name": "Amir", "age": null },
-        { "name": "Frank", "age": null },
-        { "name": "Amy", "age": null }
-    ]).getTable();
-    var expected = 
-"| name  |\n\
+test('.getTable() with empty column 2', function (t) {
+  var result = gfmt([
+    { 'name': 'Lloyd', 'age': null },
+    { 'name': 'Roger', 'age': null },
+    { 'name': 'Amir', 'age': null },
+    { 'name': 'Frank', 'age': null },
+    { 'name': 'Amy', 'age': null }
+  ]).getTable()
+  var expected =
+  '| name  |\n\
 | ----- |\n\
 | Lloyd |\n\
 | Roger |\n\
 | Amir  |\n\
 | Frank |\n\
-| Amy   |\n";
-    t.strictEqual(result, expected);
-    t.end(); 
-});
+| Amy   |\n'
+  t.strictEqual(result, expected)
+  t.end()
+})
